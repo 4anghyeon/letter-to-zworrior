72369484190c69788848a4929a3de2a7d8bb6f16
sanghyeon <uphyeon@kakao.com>
2023-11-16 20:48:04 +0900

    add font

--- src/shared/GloabalStyle.jsx
+++ src/shared/GloabalStyle.jsx
@@ -53,21 +53,21 @@ export const GlobalStyle = createGlobalStyle`
   }
   
   body {
-    //@font-face {
-    //  font-family: 'KOTRAHOPE';
-    //  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2') format('woff2');
-    //  font-weight: normal;
-    //  font-style: normal;
-    //}
-    //
-    //@font-face {
-    //  font-family: 'NPSfontBold';
-    //  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2310@1.0/NPSfontBold.woff2') format('woff2');
-    //  font-weight: 700;
-    //  font-style: normal;
-    //}
-    //
-    //font-family: 'NPSfontBold',serif;
+    @font-face {
+      font-family: 'KOTRAHOPE';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2') format('woff2');
+      font-weight: normal;
+      font-style: normal;
+    }
+
+    @font-face {
+      font-family: 'NPSfontBold';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2310@1.0/NPSfontBold.woff2') format('woff2');
+      font-weight: 700;
+      font-style: normal;
+    }
+
+    font-family: 'NPSfontBold',serif;
     
     ::-webkit-scrollbar {
       width: 5px;
